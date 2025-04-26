bceda3603743f5ff2279c311638771fe9131487f
DarshanCHMSR <chdarshan99@gmail.com>
2025-04-26 22:45:28 +0530

    the nav

--- writers_studio/src/App.jsx
+++ writers_studio/src/App.jsx
@@ -1,55 +1,64 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <div className="header_section">
-         <div className="container-fluid">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-               <div className="logo">
-                  <a href="index.html">
-                     <img src="images/logo.png" alt="Logo" />
+        <div className="container-fluid">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="logo">
+              <a href="index.html">
+                <img src="images/logo.png" alt="Logo" />
+              </a>
+            </div>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="index.html">
+                    Home
                   </a>
-               </div>
-               <button 
-                  className="navbar-toggler" 
-                  type="button" 
-                  data-bs-toggle="collapse" 
-                  data-bs-target="#navbarNav" 
-                  aria-controls="navbarNav" 
-                  aria-expanded="false" 
-                  aria-label="Toggle navigation"
-               >
-                  <span className="navbar-toggler-icon"></span>
-               </button>
-               <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav ms-auto">
-                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="index.html">Home</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="about.html">About Us</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="gallery.html">Gallery</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="services.html">Services</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="contact.html">Contact Us</a>
-                     </li>
-                     <li className="nav-item">
-                        <a className="nav-link" href="#">
-                           <i className="fa fa-search" aria-hidden="true"></i>
-                        </a>
-                     </li>
-                  </ul>
-               </div>
-            </nav>
-         </div>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="about.html">
+                    About Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="gallery.html">
+                    Gallery
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="services.html">
+                    Services
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="contact.html">
+                    Contact Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <button className="btn btn-primary">Click Me</button>
       </div>
     </>
   )
