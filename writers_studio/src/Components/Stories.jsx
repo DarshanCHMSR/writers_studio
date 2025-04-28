9cadfabe27f33d3ebecb07fb9a4ddc215aaf4feb
DarshanCHMSR <chdarshan99@gmail.com>
2025-04-29 01:11:01 +0530

    the nav

--- writers_studio/src/Components/Stories.jsx
+++ writers_studio/src/Components/Stories.jsx
@@ -8,7 +8,7 @@ const Stories = () => {
    <div className="card">
   <span className="card__title">Author : the one</span>
   <p className="card__content">
-    this is story is about the god of the world and how he created the world and how he created the humans and how he created the animals and how he created the plants and how he created the water and how he created the air and how he created the fire and how he created the earth and how he created the sky and how he created the stars and how he created the moon and how he created the sun and how he created the universe and how he created the multiverse and how he created the time and how he created the space and how he created the matter and how he created the energy and how he created the light and how he created the darkness.
+    this is story is about the god of the world and how he created the world and how he created the humans and how he created the animals and how he created the plants and how he created the water and how he created the air and how he created the fire 
   </p>
   <form className="card__form">
  
