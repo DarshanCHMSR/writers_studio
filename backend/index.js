4ea62f566b5aab06394a16f779156cce5c0db2cf
DarshanCHMSR <chdarshan99@gmail.com>
2025-05-01 23:14:19 +0530

    health

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
   res.send('Welcome to the Writers Studio API');
 });
 app.use("/api/auth", authRoutes);
