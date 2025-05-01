a554c0ff3373ce71c3cd2d39e2b8153f48593daa
DarshanCHMSR <chdarshan99@gmail.com>
2025-05-01 20:10:00 +0530

    the url

--- writers_studio/src/Components/data-link/url.jsx
+++ writers_studio/src/Components/data-link/url.jsx
@@ -1,2 +1,2 @@
-export const url  = 'http://localhost:5000'
-// export const url = 'https://writers-studio-5yaj.vercel.app/'
+// export const url  = 'http://localhost:5000'
+export const url = 'https://writers-studio-5yaj.vercel.app/'
